f501a5fb5f85fbbac8cf96daca2bb3596bfab0e1
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-11-27 03:43:07 +0700

    fix: weird ass

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react"
+import hero from '../assets/images/profile/hero.png'
 import Socials from "../components/SocialIcons"
 import Button from "../components/Button"
 
 function Hero() {
   return (
-    <section id="hero" className="pt-36 bg-my-navy text-my-white bg-network bg-center lg:bg-left">
+    <section id="hero" className="pt-36 bg-my-navy text-my-white bg-network bg-center lg:bg-left md:pb-[6.25rem]">
       <div className="container">
         <div className="flex flex-wrap">
           <div className="w-full self-center px-4 lg:w-1/2">
@@ -25,13 +26,14 @@ function Hero() {
               <Socials/>
             </div>
           </div>
-          <div className="w-full self-start px-4 lg:w-1/2">
-            <div className="mt-10 relative lg:mt-0 lg:right-0 h-[400px]">
-              <span className="absolute -bottom-0 -z-0 left-1/2 -translate-x-1/2 md:scale-125">
-                <svg width="400" height="400" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                  <path fill="#FF782D" d="M31.9,-32.2C43.6,-20.2,56.8,-10.1,62.7,5.8C68.5,21.7,66.9,43.5,55.2,52.3C43.5,61.1,21.7,56.9,6.3,50.7C-9.2,44.4,-18.4,36,-32.2,27.2C-46,18.4,-64.5,9.2,-69.2,-4.7C-73.9,-18.6,-64.8,-37.2,-51,-49.1C-37.2,-61,-18.6,-66.2,-4.2,-62C10.1,-57.8,20.2,-44.1,31.9,-32.2Z" transform="translate(100 100) scale(1.3)"/>
+          <div className="w-full self-end px-4 lg:w-1/2">
+            <div className="relative mt-10 lg:mt-9 lg:right-0 h-[400px]">
+              <span className="absolute -bottom-0 left-1/2 z-0 -translate-x-1/2 md:scale-125">
+                <svg viewBox="0 0 200 200" height="400" width="400" xmlns="http://www.w3.org/2000/svg">
+                  <path fill="#FF782D" d="M34.3,-65.3C43.6,-54.1,49.5,-43.1,57.6,-32.2C65.8,-21.3,76.1,-10.7,79.4,1.9C82.8,14.6,79.2,29.1,70.4,38.7C61.5,48.2,47.3,52.7,34.6,54.1C22,55.5,11,53.9,-1.8,56.9C-14.5,60,-29.1,67.8,-41.7,66.3C-54.3,64.9,-64.9,54.3,-71.7,41.7C-78.5,29.1,-81.4,14.5,-80.1,0.8C-78.7,-13,-73.1,-26,-63.2,-33.3C-53.4,-40.6,-39.4,-42.3,-28.2,-52.4C-17,-62.4,-8.5,-80.8,2,-84.3C12.5,-87.9,25.1,-76.5,34.3,-65.3Z" transform="translate(100 100) scale(1.1)" />
                 </svg>
               </span>
+              <img src={hero} alt="devan" className="mx-auto max-w-full max-h-[500px] brightness-90 " style={{zIndex: '2'}}/>
             </div>
           </div>  
         </div>
